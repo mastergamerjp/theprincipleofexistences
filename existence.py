7d25f78f0e18742530c8f9335513c8e490d1b2f2
mastergamerjp <89471515+mastergamerjp@users.noreply.github.com>
2024-11-23 09:39:04 -0300

    Create existence.py

---
+++ existence.py
@@ -0,0 +1,31 @@
+class VitalExistence:
+    def __init__(self):
+        pass
+    
+class DeadExistence:
+    def __init__(self):
+        pass
+
+class ArcaneExistence:
+    def __init__(self):
+        pass
+
+class StaticExistence:
+    def __init__(self):
+        pass
+    
+class FluidExistence:
+    def __init__(self):
+        pass
+    
+class WiseExistence:
+    def __init__(self):
+        pass
+
+class PsychicExistence:
+    def __init__(self):
+        pass
+
+class SpiritualExistence:
+    def __init__(self, manifestativePotential=0, SpiritReference=None):
+        pass
